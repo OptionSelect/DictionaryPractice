133184295ba8f3dade1b9ba9ad0217667e0d5419
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-22 09:32:40 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DictionaryPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Please enter the phone number associated with {name}:");
+            var phoneNumber = Console.ReadLine();
+        }
+    }
+}
